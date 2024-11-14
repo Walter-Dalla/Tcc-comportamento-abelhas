bdff020310cf66b1495cfefbbaf02cc66d0871bd
Walter-Dalla <walterdallaprofissional@gmail.com>
2024-11-14 20:54:24 -0300

    debug
    Signed-off-by: Walter-Dalla <walterdallaprofissional@gmail.com>

--- src/Modules/BasicModule/backgroundRemoveModule.py
+++ src/Modules/BasicModule/backgroundRemoveModule.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 
-def remove_background(raw_warpped_frames):
+def remove_background(raw_warpped_frames, debug_mode, is_side):
+    resume_debug_mode = False
     selected_random_frames = []
     
     frame_block = 500
@@ -24,6 +25,22 @@ def remove_background(raw_warpped_frames):
             maior_contorno = max(contornos, key=cv2.contourArea)
             cv2.drawContours(frame, [maior_contorno], -1, (0, 255, 0), 2)
         
+        
         frames.append(diff)
+        
+        if(debug_mode and not resume_debug_mode):
+            
+            frame_name = 'side' if is_side else 'top'
+            
+            cv2.imshow(frame_name+"_dif_frame", dif_frame)
+            cv2.imshow(frame_name+"_diff", diff)
+            cv2.imshow(frame_name+"_frame", frame)
+            
+            key = cv2.waitKey(0) & 0xFF
+            
+            if key == ord('n'):  # 'n' key
+                continue
+            elif key == 27:  # ESC key
+                resume_debug_mode = True
         
     return frames
--- src/Modules/BasicModule/processVideoModule.py
+++ src/Modules/BasicModule/processVideoModule.py
@@ -6,21 +6,41 @@ from src.Modules.BasicModule.topAnalizer import analyze_frame_top
 from src.Modules.BasicModule.perspectiveModule import process_perspective
 from src.Modules.ExportModule.videoUtils import open_video
 
-def process_video(frame_points, input_video_path, is_side):
+import cv2
+import time
+
+def process_video(frame_points, input_video_path, is_side, debug_mode):
+    start_time = time.time()
+    
     success, originalVideo = open_video(input_video_path)
     if not success:
         return False, None, None
+    time_open_video = time.time()
+    
     fps = int(originalVideo.get(cv2.CAP_PROP_FPS))
+    time_get_fps = time.time()
+    
     raw_warpped_frames = process_perspective(originalVideo, frame_points)
-    frames = remove_background(raw_warpped_frames)
+    time_process_perspective = time.time()
+    
+    frames = remove_background(raw_warpped_frames, debug_mode, is_side)
+    time_remove_background = time.time()
 
     data = []
-    
     if is_side:
         data = analyze_frame_side(frames)
     else:
         data = analyze_frame_top(frames)
-    
+    time_analyze_frames = time.time()
+
+    if(debug_mode):
+        print(f"Time to open video: {time_open_video - start_time:.4f} seconds")
+        print(f"Time to get FPS: {time_get_fps - time_open_video:.4f} seconds")
+        print(f"Time to process perspective: {time_process_perspective - time_get_fps:.4f} seconds")
+        print(f"Time to remove background: {time_remove_background - time_process_perspective:.4f} seconds")
+        print(f"Time to analyze frames: {time_analyze_frames - time_remove_background:.4f} seconds")
+        print(f"Total time: {time_analyze_frames - start_time:.4f} seconds")
+
     success = True
     
     return success, frames, fps, data, raw_warpped_frames
