42e9ccadfc2258380a346fee411f0416ec8b6e9e
Walter-Dalla <walterdallaprofissional@gmail.com>
2024-12-15 15:38:59 -0300

    update speed module
    Signed-off-by: Walter-Dalla <walterdallaprofissional@gmail.com>

--- src/Modules/MetadataModule/speedModule.py
+++ src/Modules/MetadataModule/speedModule.py
@@ -2,28 +2,35 @@ import math
 
 def module_call(data):
     previusPoint = None
-    
-    fps = data["fps"]
     pixel_to_cm_ratio = float(data["pixel_to_cm_ratio"])
     
-    distance = 0
-    speed = []
+    distanceTotal = 0
+    speedTotal = 0
+    speedObj = {}
+    
     for index in data["route"]:
-        value = data["route"][index]
+        routePoint = data["route"][index]
+        point = route_point_to_math_point(routePoint)
         
-        point = [value['x'], value['y'], value['z']]
-        
-        if(previusPoint == None):
+        if(index == '0'):
             previusPoint = point
             continue
         
-        distance += math.dist(previusPoint, point) * pixel_to_cm_ratio/100
+        distance = math.dist(previusPoint, point) * pixel_to_cm_ratio/100
         
-        if(int(index) % fps == 0):
-            speed.append(distance/pixel_to_cm_ratio)
-            distance = 0
-    
-    data["speed"] = speed
+        speed = distance/pixel_to_cm_ratio
+        speedObj[index] = speed
+        speedTotal += speed
+        distanceTotal += distance
+        
+        previusPoint = point
+        
+    data["speed"] = speedObj
+    data["averageSpeed"] = speedTotal / len(data["route"])
+    data["distanceTotal"] = distanceTotal
     
     return data
+
+def route_point_to_math_point(routePoint):
+    return [routePoint['x'], routePoint['y'], routePoint['z']]
 
